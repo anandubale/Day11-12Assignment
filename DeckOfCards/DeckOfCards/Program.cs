982eef20738032106c546166dd2d9aba78c886f2
anandubale <au26199@gmail.com>
2021-12-25 13:58:09 +0530

    Added Deck OF card Program

---
+++ DeckOfCards/DeckOfCards/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DeckOfCards
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Deck Of Cards!");
+            Class1 cards = new Class1();
+            cards.CardsCreator();
+
+
+
+        }
+    }
+}
